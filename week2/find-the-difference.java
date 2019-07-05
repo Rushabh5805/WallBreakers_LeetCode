8450ab9a13f8a521ac260f1b66999a147b824e49
Rushabh5805 <38410628+Rushabh5805@users.noreply.github.com>
2019-07-05 13:27:34 -0700

    Create find-the-difference.java

---
+++ week2/find-the-difference.java
@@ -0,0 +1,16 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char ch : s.toCharArray()){
+        	map.put(ch,map.getOrDefault(ch, 0) + 1);
+        }
+        for(char ch : t.toCharArray()){
+        	map.put(ch,map.getOrDefault(ch, 0) + 1);
+        }
+        for(Map.Entry<Character, Integer> e : map.entrySet()){
+            if(e.getValue()%2!=0)
+                return e.getKey();
+        }
+        return '1';
+    }
+}
