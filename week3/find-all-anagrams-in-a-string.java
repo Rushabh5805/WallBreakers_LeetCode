53dc2b35f7844380561805a4cfc79d0d8d8c7757
Rushabh5805 <38410628+Rushabh5805@users.noreply.github.com>
2019-07-14 18:10:33 -0700

    Create find-all-anagrams-in-a-string.java

---
+++ week3/find-all-anagrams-in-a-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        
+        int x=p.length();
+        List<Integer> l = new ArrayList<>();
+        int r=s.length();
+        p = new String(xy(p.toCharArray()));
+        for(int i=0;i<r-x+1;i++){
+            String y = new String(xy(s.substring(i,i+x).toCharArray()));
+            if(y.equals(p))
+                l.add(i);
+        }        
+        return l;
+    }
+    public char[] xy(char[] x){
+        Arrays.sort(x);
+        return x;
+    }
+}
